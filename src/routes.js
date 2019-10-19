df5bd52b27bd4ccf07c6403f85b809e2e4c2923c
Wellici Araujo <wja1@ifal.edu.br>
2019-10-19 11:07:19 -0300

    todas as rotass com seus respectivos controllers

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,16 @@
 import { Router } from "express";
+import ProjectController from "./controllers/ProjectController";
 
 const routes = new Router();
 
-routes.post("/projects", (req, res) => {
-  return res.json({ ok: true });
-});
+routes.post("/projects", ProjectController.store);
 
-routes.get("/projects", (req, res) => {
-  return res.json({ ok: true });
-});
+routes.get("/projects", ProjectController.index);
+
+routes.get("/projects/:id", ProjectController.show);
+
+routes.put("/projects/:id", ProjectController.update);
+
+routes.delete("/projects/:id", ProjectController.delete);
 
 export default routes;
